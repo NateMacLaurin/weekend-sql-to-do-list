d33614ae87e84d2b1751fd348c77f5de02f7294d
NateMacLaurin <brow1982@umn.edu>
2021-01-11 08:14:51 -0600

    jq sourced and click handler setup on add button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,22 @@
 //jq and js goes here to interface with dom, and use ajax calls to POST GET DELETE PUT data from express server
+
+$(document).ready(function(){
+    console.log('JQ Sourced.');
+    renderList();
+    addClickHandlers();
+});
+
+function renderList(){
+    console.log('In renderList');
+    //AJAX GET
+}; //end renderList
+
+function addClickHandlers(){
+    console.log('In addClickHandlers');
+    $('#addTaskButton').on('click', handleSubmit);
+}; //end addClickHandlers
+
+function handleSubmit(){
+    console.log('in handleSubmit');
+    //AJAX POST with data from input field on DOM
+}; //end handleSubmit
