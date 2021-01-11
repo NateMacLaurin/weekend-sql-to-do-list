86604368eee7d9a3a4cdc920b84ce0e655ec5d92
NateMacLaurin <brow1982@umn.edu>
2021-01-11 08:19:29 -0600

    express server setup on localhost:5000

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
 //localhost express server on port 5000, set up router and listener here
+
+const express = require('express');
+const bodyParser = require('body-parser');
+//const taskRouter = require('./routes/todo.router');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+
+//app.use('/tasks', taskRouter);
+
+app.use(express.static('server/public'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
