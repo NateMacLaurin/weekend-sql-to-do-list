0cf84b2009c6e9be9cfacab34f650e9c29a4f52f
NateMacLaurin <brow1982@umn.edu>
2021-01-11 07:45:21 -0600

    database created in pgadmin and tested structure with dummy data

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
 --Test database goes here
+
+CREATE TABLE todo(
+	"id" SERIAL PRIMARY KEY,
+	"task" VARCHAR (250) NOT NULL,
+	"completed" BOOLEAN DEFAULT FALSE);
+
+INSERT INTO "todo" ("task") VALUES ('Brush Teeth');
+
+SELECT * FROM "todo";
